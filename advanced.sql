ef648ecf2bbcbcfc0b76be4461abee3948c91d4a
lilyren8 <lilyren8@gmail.com>
2021-06-14 10:34:25 -0400

    update

--- advanced.sql
+++ advanced.sql
@@ -32,3 +32,73 @@ GROUP BY WEEK(created_at);
 -- conclusion: bid up for desktop got more session to the website
 -- next step: continue to monitor device-level volume and be aware of the impact of bid;
 -- continue to monitor conversion performance by device type to optimize spend
+
+
+-- pull the most viewed website pages
+SELECT pageview_url, COUNT(DISTINCT website_session_id) AS sessions
+FROM website_pageviews
+WHERE created_at < '2012-06-09'
+GROUP BY 1
+ORDER by 2 DESC;
+-- conclusion: home, products, and mr.fuzzy page gets the most views
+-- next step: does this list also represents the top entry pages?
+-- analyze the performance of each page
+
+
+-- pull the top entry page
+-- find the entry page id for each session
+CREATE TEMPORARY TABLE entry_page
+SELECT MIN(website_pageview_id) AS entry_page_id, website_session_id
+FROM website_pageviews
+WHERE created_at < '2012-06-12'
+GROUP BY website_session_id;
+
+-- count sessions for each entry page 
+SELECT pageview_url AS landing_page, COUNT(e.website_session_id) AS sessions
+FROM entry_page AS e
+JOIN website_pageviews AS w
+ON e.entry_page_id = w.website_pageview_id
+GROUP BY 1;
+-- conclusion: all traffic is through home page
+-- next step: home page performance
+-- think about if home page is the best initial experience
+
+
+-- calculate bounce rate for home page
+
+-- step 1: find the landing page for each session
+CREATE TEMPORARY TABLE landing_page
+SELECT MIN(website_pageview_id) AS landing_page_id, website_session_id
+FROM website_pageviews
+WHERE created_at < '2012-06-14'
+GROUP BY website_session_id;
+
+-- step 2: identify the session with home as the landing page
+CREATE TEMPORARY TABLE home_landing
+SELECT l.website_session_id AS session_id, pageview_url
+FROM landing_page AS l
+JOIN website_pageviews AS w
+ON  l.landing_page_id = w.website_pageview_id
+WHERE pageview_url = '/home';
+
+-- step 3: identify bounced sessions
+CREATE TEMPORARY TABLE bounced_sessions
+SELECT website_session_id AS bounced_sessions, h.pageview_url, COUNT(website_pageview_id)
+FROM home_landing AS h
+JOIN website_pageviews AS w
+ON h.session_id = w.website_session_id
+GROUP BY h.session_id, h.pageview_url
+HAVING COUNT(website_pageview_id) = 1;
+
+-- step 4: count the total and bounced sessions
+SELECT COUNT(l.website_session_id) AS sessions, 
+COUNT(b.bounced_sessions) AS bounced_sessions, 
+COUNT(b.bounced_sessions)/COUNT(l.website_session_id) AS bounce_rate
+FROM landing_page AS l
+LEFT JOIN bounced_sessions AS b
+ON l.website_session_id = b.bounced_sessions;
+
+
+
+
+
